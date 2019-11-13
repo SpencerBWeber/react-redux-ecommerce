9437780867c6baf0e304a2ea96210d2c6b7974af
SpencerBWeber <spencerweber019@gmail.com>
2019-11-12 19:16:47 -0700

    built headernavbarReducer

--- src/components/headernavbar/navbar.js
+++ src/components/headernavbar/navbar.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-export default class Navbar extends Component {
-    render() {
-        return (
-            <div className='navbar'>
-
-            </div>
-        )
-    }
+class Navbar extends Component {
+  render() {
+    return <div className="navbar"></div>;
+  }
 }
+
+export default Navbar;
